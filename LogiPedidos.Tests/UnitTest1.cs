d78f5b06778b58756227c982f4a775db6f00f126
Turgho <victor.hugo3692111@gmail.com>
2025-08-11 22:19:07 -0300

    chore: criar pasta de testes unitários

---
+++ LogiPedidos.Tests/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace LogiPedidosBackend.LogiPedidos.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
